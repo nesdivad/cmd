55296aa06fbe29347d85882cdf13b0ddbc3853d5
Kristoffer Davidsen <nesdivad@protonmail.com>
2024-05-09 17:34:17 +0200

    sammenlign filene og transformer

--- jsonsubstitution/src/main.zig
+++ jsonsubstitution/src/main.zig
@@ -7,19 +7,55 @@ fn readJsonFile(allocator: std.mem.Allocator, path: []u8) !std.json.Parsed(Value
     return std.json.parseFromSlice(Value, allocator, file, .{ .allocate = .alloc_always, .ignore_unknown_fields = true });
 }
 
-fn writeToFile(value: Value, name: []u8) !void {
-    var string = std.ArrayList(u8).init(std.heap.page_allocator);
-    try std.json.stringify(value, .{ .whitespace = .indent_4 }, string.writer());
-    var file = try std.fs.cwd().createFile(name, .{ .read = true });
-    try file.writeAll(string.items);
-    for (string.items) |item| {
-        std.debug.print("{c}", .{item});
+fn writeToFile(value: std.StringArrayHashMapUnmanaged(Value), name: []u8) !void {
+    var out = std.ArrayList(u8).init(std.heap.page_allocator);
+    var writeStream = std.json.writeStream(out.writer(), .{ .whitespace = .indent_4 });
+    defer writeStream.deinit();
+
+    try writeStream.beginObject();
+    var it = value.iterator();
+    while (it.next()) |entry| {
+        std.debug.print("\nKey: {s}, Value: {?}\n", .{ entry.key_ptr.*, entry.value_ptr.* });
+        try writeStream.objectField(entry.key_ptr.*);
+        try writeStream.write(entry.value_ptr.*);
     }
+    try writeStream.endObject();
+
+    var file = std.fs.cwd().createFile(name, .{ .read = true, .exclusive = true }) catch |err| switch (err) {
+        error.PathAlreadyExists => {
+            std.debug.print("\nFile already exists: {s}", .{name});
+            return;
+        },
+        else => {
+            std.debug.print("{?}", .{err});
+            return;
+        },
+    };
+    try file.writeAll(out.items);
+}
+
+pub fn compareAndReplace(original: Value, into: Value, allocator: std.mem.Allocator) !std.StringArrayHashMapUnmanaged(Value) {
+    var originalHMap = try std.json.ArrayHashMap(Value).jsonParseFromValue(allocator, original, .{ .allocate = .alloc_always });
+    var intoHMap = try std.json.ArrayHashMap(Value).jsonParseFromValue(allocator, into, .{ .allocate = .alloc_always });
+
+    defer originalHMap.deinit(allocator);
+    defer intoHMap.deinit(allocator);
+
+    var iter2 = intoHMap.map.iterator();
+    while (iter2.next()) |entry| {
+        _ = try originalHMap.map.put(allocator, entry.key_ptr.*, entry.value_ptr.*);
+    }
+
+    var iter = originalHMap.map.iterator();
+    while (iter.next()) |entry| {
+        std.debug.print("\nKey/Value: {s}, {?}", .{ entry.key_ptr.*, entry.value_ptr.* });
+    }
+
+    return originalHMap.map;
 }
 
 pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const allocator = gpa.allocator();
+    const allocator = std.heap.page_allocator;
 
     const argv = std.process.argsAlloc(allocator) catch |err| {
         std.debug.print("Alloc failed: {?}", .{err});
@@ -35,5 +71,10 @@ pub fn main() !void {
 
     const file2 = try readJsonFile(allocator, filepath2);
     defer file2.deinit();
-    try writeToFile(file1.value, filename);
+
+    const hashmap = compareAndReplace(file1.value, file2.value, allocator) catch |err| {
+        std.debug.print("{?}", .{err});
+        return;
+    };
+    try writeToFile(hashmap, filename);
 }
