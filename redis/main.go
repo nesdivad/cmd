a491c1c4bef716717d2aa1d75d00525f34d307eb
Kristoffer Davidsen <88896307+nesdivad@users.noreply.github.com>
2024-12-29 11:56:31 +0100

    aof storage

--- redis/main.go
+++ redis/main.go
@@ -2,9 +2,8 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"net"
-	"os"
+	"strings"
 )
 
 func main() {
@@ -15,6 +14,26 @@ func main() {
 		fmt.Println(err)
 		return
 	}
+
+	aof, err := NewAof("database.aof")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer aof.Close()
+
+	aof.Read(func(value Value) {
+		command := strings.ToUpper(value.arr[0].bulk)
+		args := value.arr[:1]
+
+		handler, ok := Handlers[command]
+		if !ok {
+			fmt.Println("Invalid request, unknown command")
+			return
+		}
+
+		handler(args)
+	})
 
 	conn, err := l.Accept()
 	if err != nil {
@@ -28,17 +47,37 @@ func main() {
 		resp := NewResp(conn)
 		value, err := resp.Read()
 		if err != nil {
-			if err == io.EOF {
-				break
-			}
-
 			fmt.Println("error reading from client: ", err.Error())
-			os.Exit(1)
+			return
 		}
 
-		_ = value
+		if value.typ != "array" {
+			fmt.Println("Invalid request, expected array")
+			continue
+		}
+
+		if len(value.arr) == 0 {
+			fmt.Println("Invalid request, expected array length > 0")
+			continue
+		}
+
+		command := strings.ToUpper(value.arr[0].bulk)
+		args := value.arr[:1]
 
 		writer := NewWriter(conn)
-		writer.Write(Value{typ: "string", str: "OK"})
+
+		handler, ok := Handlers[command]
+		if !ok {
+			fmt.Println("Invalid command: ", command)
+			writer.Write(Value{typ: "string", str: ""})
+			continue
+		}
+
+		if command == "SET" || command == "HSET" {
+			aof.Write(value)
+		}
+
+		result := handler(args)
+		writer.Write(result)
 	}
 }
